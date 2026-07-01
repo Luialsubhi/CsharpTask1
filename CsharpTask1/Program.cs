54f0c3b41d8c5fe360a0e20e28a802eff514dd42
Luialsubhi <luialsubhi77@gmail.com>
2026-07-01 12:00:39 +0400

    Even or Odd Checker

--- CsharpTask1/Program.cs
+++ CsharpTask1/Program.cs
@@ -15,9 +15,51 @@ namespace CsharpTask1
 
 
             //////////////////////////////////////////////////////////////////////////
-            
+            ///
 
-               
-        }
+            //Rectangle Calculator
+
+            Console.WriteLine("Enter the length :");
+            float length = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter the width :");
+            float width = float.Parse(Console.ReadLine());
+
+
+            float area = length * width;
+            float Perimeter = 2 * (length + width);
+
+            Console.WriteLine("AREA:" + area);
+            Console.WriteLine("Perimeter:" + Perimeter);
+
+            //////////////////////////////////////////////////////////
+            
+            //Even or Odd Checker
+
+            Console.WriteLine("enter a whole number :");
+            float number = float.Parse(Console.ReadLine());
+
+            float remainder = number % 2;
+
+            if (remainder == 0)
+            {
+                Console.WriteLine("The number is even");
+
+            }
+            else
+            {
+                Console.WriteLine("The number is odd");
+            }
+
+
+
+
+
+
+
+
+
+
+            }
     }
 }
