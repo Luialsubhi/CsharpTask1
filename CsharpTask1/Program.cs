737e4122c01e3ceaee7a4597c0acc010c887b395
Luialsubhi <luialsubhi77@gmail.com>
2026-07-01 11:29:13 +0400

    Add project files.

---
+++ CsharpTask1/Program.cs
@@ -0,0 +1,23 @@
+﻿
+namespace CsharpTask1
+ {
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Personal Info Card
+            string name = "Sara";
+            int age = 21;
+            double height = 1.65;
+            bool isStudent = true;
+
+            Console.WriteLine("Name :" + name + " , Age: "+age +", height:"+height + ", student :" +isStudent);
+
+
+            //////////////////////////////////////////////////////////////////////////
+            
+
+               
+        }
+    }
+}
