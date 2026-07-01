4095eb1ca20473dd4161fbbabc06471a7e7d0eb8
Luialsubhi <luialsubhi77@gmail.com>
2026-07-01 13:17:49 +0400

    voting eligibility  and Grade Letter Lookup

--- CsharpTask1/Program.cs
+++ CsharpTask1/Program.cs
@@ -49,17 +49,75 @@ namespace CsharpTask1
             else
             {
                 Console.WriteLine("The number is odd");
-            }
-
-
-
-
-
-
-
-
 
 
             }
+
+            /////////////////////////////////////////////////////////////////////////
+
+            //voting eligibility
+
+            Console.WriteLine("enter your age :");
+            int age1 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("whether they hold a valid national");
+            string nationalID = Console.ReadLine();
+            bool withNationalID =( nationalID == "yes");
+
+            if  (age1 >= 18 && withNationalID)
+            {
+                Console.WriteLine("You are eligible to vote");
+            }
+            else
+            {
+                Console.WriteLine("You are not eligible to vote");
+            }
+
+            ////////////////////////////////////////////////////////////////////////
+
+            //Grade Letter Lookup
+
+            Console.WriteLine("Enter your grade letter :");
+            Console.WriteLine("A");
+            Console.WriteLine("B");
+            Console.WriteLine("C");
+            Console.WriteLine("D");
+            Console.WriteLine("F");
+
+            Console.WriteLine("please choose your grade letter from the above options :");
+            int option = int.Parse(Console.ReadLine());
+
+            switch (option)
+            {
+                case 1:
+                    Console.WriteLine("Excellent");
+                    break;
+                case 2:
+                    Console.WriteLine("Good");
+                    break;
+                case 3:
+                    Console.WriteLine("Average");
+                    break;
+                case 4:
+                    Console.WriteLine("Below Average");
+                    break;
+                case 5:
+                    Console.WriteLine("Failing");
+                    break;
+                default:
+                    Console.WriteLine("Invalid option");
+                    break;
+            }
+            /////////////////////////////////////////////////////////////////////////
+               
+                
+            
+                
+
+            
+
+
+
+        }
     }
 }
