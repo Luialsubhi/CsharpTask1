9c39d9a77db9609b5bfb38c98f809a4d9cea71a4
Luialsubhi <luialsubhi77@gmail.com>
2026-07-02 15:15:51 +0400

    multiplication table

---
+++ Csharptask2/Program.cs
@@ -0,0 +1,48 @@
+﻿namespace task2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           // countdown Timer
+           Console.WriteLine("enter a starting number :");
+            int  sN = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = sN; i >= 1; i--) 
+            {
+                Console.WriteLine(i);
+                
+            }
+            //////////////////////////////////////////////////////////////
+
+            //sum of numbers from 1 to N
+            Console.WriteLine("enter a positive whole number"); 
+            int N = Convert.ToInt32(Console.ReadLine());
+
+            int sum = 0;
+
+            for (int i = 1; i <= N; i++)
+            {
+               
+                sum += i;
+
+            }
+            Console.WriteLine("sum ="+ sum);
+
+            //////////////////////////////////////////////////////////////////
+
+            //multiplication table 
+
+            Console.WriteLine("enter a number to get its multiplication table");
+            int num = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 1; i <= 10; i++)
+            {
+                Console.WriteLine(num + " x " + i + " = " + (num * i));
+            }
+            /////////////////////////////////////////////////////////////////////
+            
+
+        }
+    }
+}
